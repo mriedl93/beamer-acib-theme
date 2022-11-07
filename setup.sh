fa28b76ea06d309723cfb2429fa109cf9cb1bbae
Markus Riedl <markus.riedl@boku.ac.at>
2022-11-07 14:44:29 +0100

    update poster template

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ln -s beamer-acib-theme/beamerthemeacib.sty beamerthemeacib.sty
-ln -s beamer-acib-theme/beamercolorthemeacib.sty beamercolorthemeacib.sty
+ln -s beamer-acib-theme/beamerthemeacib.sty .
+ln -s beamer-acib-theme/beamercolorthemeacib.sty .
+ln -s beamer-acib-theme/beamerthemeacibposter.sty .
 ln -s beamer-acib-theme/Makefile .
 ln -s beamer-acib-theme/logos .
