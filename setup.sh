2741239273df80abe658eed6f30ef7fe2a0c963d
Markus Riedl <markus.riedl@boku.ac.at>
2023-02-07 12:16:22 +0100

    adapt to tectonic structure

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ln -s beamer-acib-theme/beamerthemeacib.sty .
-ln -s beamer-acib-theme/beamercolorthemeacib.sty .
-ln -s beamer-acib-theme/beamerthemeacibposter.sty .
-ln -s beamer-acib-theme/Makefile .
-ln -s beamer-acib-theme/logos .
+ln -s ../beamer-acib-theme/beamerthemeacib.sty src/
+ln -s ../beamer-acib-theme/beamercolorthemeacib.sty src/
+ln -s ../beamer-acib-theme/beamerthemeacibposter.sty src/
+ln -s ../beamer-acib-theme/Makefile src/
+ln -s ../beamer-acib-theme/logos src/
